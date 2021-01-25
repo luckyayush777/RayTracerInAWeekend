f3320e3bd1524b04c21ee08bf4ae8e07f48fb4b5
Ayush Kumar <ayushrocks456@gmail.com>
2021-01-25 22:18:44 +0530

    256 changed to 512 for git testing

--- RayTracingInOneWeekend/Source.cpp
+++ RayTracingInOneWeekend/Source.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-	int imageWidth = 256;
-	int imageHeight = 256;
+	int imageWidth = 512;
+	int imageHeight = 512;
 	std::cout << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
 	for (int i = imageHeight - 1; i >= 0; i--)
 	{
@@ -13,9 +13,9 @@ int main()
 			float r = float(i) / float(imageWidth);
 			float g = float(j) / float(imageHeight);
 			float b = 0.25;
-			int ir = int(255.99 * r);
-			int ig = int(255.99 * g);
-			int ib = int(255.99 * b);
+			int ir = int(511.99 * r);
+			int ig = int(511.99 * g);
+			int ib = int(511.99 * b);
 			std::cout << ir << " " << ig << " " << ib << "\n";
 
 		}
